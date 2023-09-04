063f1f74f829ee06a0729d57cb1024c12bc659be
Anyx <anyxp@protonmail.com>
2023-09-04 03:57:38 +0300

    add suspension check to login

--- api-client.php
+++ api-client.php
@@ -239,6 +239,12 @@ class VistapanelApi
             throw new Exception("Unable to login.");
         }
         if (
+            strpos($login, "document.location.href = 'panel/index_pl_sus.php") ===
+            false
+        ) {
+         throw new Exception("Your account is suspended.");
+        }
+        if (
             strpos($login, "document.location.href = 'panel/indexpl.php") ===
             false
         ) {
